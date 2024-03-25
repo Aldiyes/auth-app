c98bcedbb542e698de1c49c9665890b44c72f94f
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-03-25 19:42:18 +0700

    create email verification token

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,6 @@
+import { NewVerificationForm } from '@/components/auth/new-verification-form';
+
+const NewVefiricationPage = () => {
+	return <NewVerificationForm />;
+};
+export default NewVefiricationPage;
